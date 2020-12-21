43f71ac54f243eba59b42dae8fa5ff3479dedeb8
SamElmore625 <54898090+SamElmore625@users.noreply.github.com>
2020-12-21 08:32:55 -0500

    Worked on Setting up UI

---
+++ HiNoteProject/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  HiNoteProject
+//
+//  Created by Sam Elmore on 12/21/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+}
+
